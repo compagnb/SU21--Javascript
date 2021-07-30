6c50e9f6647b7fabb1df85307435eb7df725bdfb
Barbara Compagnoni <bcphotodesigns@aol.com>
2021-07-30 19:35:26 -0400

    js

---
+++ js/hw_wk3_rockspaperscissor.js
@@ -0,0 +1,21 @@
+// Function to hide start div and show end div
+let startContainer = document.getElementById('startcontainer');
+let btn = document.querySelector("#startbutton");
+
+btn.addEventListener('click', function(){
+    startContainer.style.opacity = 0;
+    startContainer.style.transform = 'scale(0)';
+    // Add timeout with length matching animation-duration
+    window.setTimeout(function(){
+        startContainer.style.display = 'none';
+    },700);
+});
+
+function hideEndContainerShowWinner() {
+    // In this function create a 2 variables for :
+    //     class gamecomtainer (.gamecontainer) using querySelector
+    //     Id bottomcontainer using getElementByID.
+    // Once the variables are created have the game container disappear.
+}
+
+// create similar to the above create variables for rockdiv, paperdiv, scissorsdiv, computerrockdiv, computerpaperdiv, computerscissorsdiv
