7bbc2598043665a05062458d63e7eea90831840b
Barbara Compagnoni <bcphotodesigns@aol.com>
2021-08-04 20:50:28 -0400

    Update hw_wk3_rockspaperscissor.js

--- js/hw_wk3_rockspaperscissor.js
+++ js/hw_wk3_rockspaperscissor.js
@@ -1,6 +1,6 @@
 // Function to hide start div and show end div
-let startContainer = document.getElementById('startcontainer');
-let btn = document.querySelector("#startbutton");
+var startContainer = document.getElementById('startcontainer');
+var btn = document.querySelector("#startbutton");
 
 btn.addEventListener('click', function(){
     startContainer.style.opacity = 0;
@@ -12,11 +12,109 @@ btn.addEventListener('click', function(){
 });
 
 function hideEndContainerShowWinner() {
-    // In this function create 2 variables for :
-    //     class gamecomtainer (.gamecontainer) using querySelector
-    //     Id bottomcontainer using getElementByID.
-    // Once the variables are created have the game container disappear.
+    var gamecontainer = document.querySelector(".gamecontainer");
+    var bottomcontainer = document.getElementById('bottomcontainer');
+    gamecontainer.style.opacity = 0;
+    gamecontainer.style.transform = 'scale(0)';
+    gamecontainer.style.display = 'none';
+    bottomcontainer.style.display = 'block';
 }
 
-// create similar to the above create variables for rockdiv, paperdiv, scissorsdiv, computerrockdiv, computerpaperdiv, computerscissorsdiv
-const rockSelected = document.querySelector("#rockdiv");
+
+//Game Starts Here!
+//Global Variables
+
+var playerScore = 0;
+var computerScore = 0;
+var gamesPlayed = 0;
+
+document.addEventListener("click", gameSelectionListener);
+
+function gameSelectionListener(event){
+    var element = event.target;
+    console.log(event.target);
+
+    var rock = "rock";
+    var paper = "paper";
+    var scissors = "scissors";
+
+    if(element.classList.contains("gameselection") && element.id ===("rockdiv")){
+        playRound(rock);
+        console.log("rock submitted");
+    }else if(element.classList.contains("gameselectiontext") && element.id === ("rockselection")){
+        playRound(rock);
+        console.log("rock submitted");
+    }else if(element.classList.contains("selection") && element.id === ("rockimg")){
+        playRound(rock);
+        console.log("rock submitted");
+    }else if (element.classList.contains("gameselection") && element.id ===("paperdiv")){
+        playRound(paper);
+        console.log("paper submitted");
+    }
+    // homework finish paper, and complete scissors
+
+
+
+
+}
+
+
+function computerPlay(){
+    randomGameValue = Math.floor(Math.random() * 3);
+    if(randomGameValue == "0"){
+        return "rock";
+    }else if(randomGameValue == "1"){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection){
+    console.log(playerSelection);
+
+    const computerSelection  = computerPlay();
+    const rockSelected = document.querySelector("#rockdiv");
+    const scissorsSelected = document.querySelector("#scissorsdiv");
+    const paperSelected = document.querySelector("#paperdiv");
+    const computerRockSelected = document.querySelector("#computerrockdiv");
+    const computerScissorsSelected = document.querySelector("#computerscissorsdiv");
+    const computerPaperSelected = document.querySelector("#computerpaperdiv");
+
+    if(computerSelection == "rock"){
+        computerRockSelected.style.backgroundColor = "#7987e9";
+        computerScissorsSelected.style.backgroundColor = "white";
+        computerPaperSelected.style.backgroundColor = "white";
+    }else if(computerSelection == "paper"){
+        computerRockSelected.style.backgroundColor = "white";
+        computerScissorsSelected.style.backgroundColor = "white";
+        computerPaperSelected.style.backgroundColor = "#7987e9";
+    }else if(computerSelection == "scissors"){
+        computerRockSelected.style.backgroundColor = "white";
+        computerScissorsSelected.style.backgroundColor = "#7987e9";
+        computerPaperSelected.style.backgroundColor = "white";
+    }
+
+    var playerSelectionString = String(playerSelection);
+    var playerSelectionLowercase = playerSelectionString.toLowerCase();
+
+    if(playerSelectionLowercase === "rock"){
+        rockSelected.style.backgroundColor = "#fc5868";
+        paperSelected.style.backgroundColor = "white";
+        scissorsSelected.style.backgroundColor = "white";
+        if (computerSelection === "rock"){
+            console.log("Tied!!");
+        }else if(computerSelection === "paper"){
+            console.log("Computer Wins!");
+        }else if(computerSelection === "scissors"){
+            console.log("Players Wins!");
+        }
+    }else if(playerSelectionLowercase === "paper"){
+
+    }else if(playerSelectionLowercase === "scissors"){
+        
+    }
+
+
+}
+
